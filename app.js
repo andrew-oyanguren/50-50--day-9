f7e08999269a50f923fecf4404df72349c69ac5e
Andrew <aaoyanguren@outlook.com>
2021-01-03 14:46:58 -0800

    Updated and completed app.js

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 // Variables
-
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
 
 // Event Listeners
 
-
+jokeBtn.addEventListener('click', generateJoke);
 
 // Functions
+
+generateJoke();
+//USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  const response = await fetch('https://icanhazdadjoke.com', config)
+  const data = await response.json()
+
+  jokeEl.innerHTML = data.joke;
+}
+
+// USING .THEN
+/*function generateJoke() {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  fetch('https://icanhazdadjoke.com', config)
+    .then((response) => response.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+}*/
